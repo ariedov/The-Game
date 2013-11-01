98d47d6b1196024de81a167703d7568fcdc451f0
David <ariedov@gmail.com>
2013-11-01 20:49:34 +0200

    made command help

--- commands.js
+++ commands.js
@@ -10,7 +10,7 @@ var helpText = 	"pick - picks up a item from floor.\n" +
 		"enter - enters the door.\n"+
 		"say <smth> - prints smth to the console.\n"+
 		"inventory - shows your inventory\n"+
-		"pick <name> - picks item from inverntory\n"+
+		"choose <name> - picks item from inverntory\n"+
 		"hand - shows item in your hand\n"+
 		"name <text> - sets user name\n"+
 		"\n"+
@@ -20,7 +20,31 @@ var helpText = 	"pick - picks up a item from floor.\n" +
 
 commandActions = {
 	"name": function(name) { user = name; return "Name changed to " + user; },
-	"help": function() { return helpText; },
+	"help": function(param) { 
+		var commandHelp = {
+			"pick": "picks up a item from floor",
+			"drop": "drops item to the floor",
+			"use": "uses item action",
+			"go": "moves one step to choosed direction",
+			"forward": "rotates forward",
+			"back": "rotates back",
+			"left": "rotates left",
+			"right": "rotates right",
+			"enter": "enters the door",
+			"say <smth>": "prints smth to console",
+			"inventory": "shows your inventory",
+			"choose <name>": "picks item from inventory",
+			"hand": "shows item in your hand",
+			"name <text>": "sets user name",
+			"help": "shows main help for the game",
+		}
+
+		var helpString = commandHelp[param];
+		if (helpString != undefined) {
+			return param + " - " + helpString;
+		}
+		return helpText;
+	},
 }
 
 function startCommand(command, param) {
