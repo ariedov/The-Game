a6e841a52b95d5dd75e0f5d1c56abc840b1405a3
David <ariedov@gmail.com>
2013-11-02 18:00:02 +0200

    preparing level for movement

--- level1.js
+++ level1.js
@@ -7,3 +7,18 @@ function Level1() {
 Level1.prototype.load = function() {
 	battlefield.value += welcomeText;
 }
+
+Level1.prototype.left = function() {
+}
+
+Level1.prototype.right = function() {
+}
+
+Level1.prototype.up = function() {
+}
+
+Level1.prototype.down = function() {
+}
+
+Level1.prototype.go = function() {
+}
